97a79a3f62963b5bad370df04f4a5dab41037fbc
Daniil Grishaev <grishaev.daniil1@gmail.com>
2020-11-20 08:08:28 +0300

    Replaced fake image with current film image on filmsBoard page

--- src/components/related/BulletinBoard/Board/Card/component.jsx
+++ src/components/related/BulletinBoard/Board/Card/component.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Wrapper, BottomInfo, Title } from "./styles";
 
 const component = ({ film }) => (
-  <Wrapper to={`film/${film.id}`} src="https://picsum.photos/800/1200">
+  <Wrapper to={`film/${film.id}`} src={film.image}>
     <BottomInfo>
       <Title>{film.title}</Title>
     </BottomInfo>

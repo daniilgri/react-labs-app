a3e2f83d451aa46cffe7225226e83648e8f58eb7
Daniil Grishaev <grishaev.daniil1@gmail.com>
2020-12-01 10:10:18 +0300

    eslint+prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    $: "readonly",
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": "off",
+    "func-names": "off",
+  },
+};

127bb942d46b4fb1257c8068ae470b60971f2a32
Daniil Grishaev <grishaev.daniil1@gmail.com>
2020-12-10 12:08:23 +0300

    Unit-testing for cancelOrder saga

--- src/store/sagas/__tests__/ordersSaga.test.js
+++ src/store/sagas/__tests__/ordersSaga.test.js
@@ -1,13 +1,18 @@
 import "firebase/storage";
 import { runSaga } from "redux-saga";
 
-import { makeOrder, fetchOrdersInitial } from "../ordersSaga";
+import { makeOrder, fetchOrdersInitial, fetchOrdersNext, cancelOrder } from "../ordersSaga";
 import * as api from "../../../services/ordersAPI";
 import {
   makeOrderSucceed,
   makeOrderFailed,
   fetchOrdersInitialSucceed,
   fetchOrdersInitialFailed,
+  fetchOrdersInitialRequested,
+  fetchOrdersNextFailed,
+  fetchOrdersNextSucceed,
+  cancelOrderFailed,
+  cancelOrderSucceed,
 } from "../../actions/ordersActions";
 
 describe("makeOrder saga", () => {
@@ -64,10 +69,20 @@ describe("makeOrder saga", () => {
 
 describe("fetchOrdersInitial saga", () => {
   it("should fetch orders initial and dispatch success action", async () => {
+    const fakeOrdersData = {
+      orders: [
+        {
+          film: { title: "title" },
+          screeningDate: { date: "00.00.0000", time: "00:00" },
+          userUid: "",
+        },
+      ],
+      allCount: 1,
+    };
     const dispatched = [];
     const getOrdersInitialApi = jest
       .spyOn(api, "getOrdersInitialAPI")
-      .mockImplementation(() => Promise.resolve());
+      .mockImplementation(() => Promise.resolve(fakeOrdersData));
 
     await runSaga(
       {
@@ -79,12 +94,12 @@ describe("fetchOrdersInitial saga", () => {
     );
 
     expect(getOrdersInitialApi).toHaveBeenCalledTimes(1);
-    expect(dispatched).toEqual([fetchOrdersInitialSucceed()]);
+    expect(dispatched).toEqual([fetchOrdersInitialSucceed(fakeOrdersData)]);
 
     getOrdersInitialApi.mockClear();
   });
 
-  it("should fetch orders initial and dispatch success action", async () => {
+  it("should fetch orders initial and dispatch error action", async () => {
     const dispatched = [];
     const getOrdersInitialApi = jest
       .spyOn(api, "getOrdersInitialAPI")
@@ -103,5 +118,101 @@ describe("fetchOrdersInitial saga", () => {
     expect(dispatched).toEqual([fetchOrdersInitialFailed()]);
 
     getOrdersInitialApi.mockClear();
+  });
+});
+
+describe("fetchOrdersNext saga", () => {
+  it("should fetch orders next and dispatch success action", async () => {
+    const fakeOrdersData = {
+      orders: [
+        {
+          film: { title: "title" },
+          screeningDate: { date: "00.00.0000", time: "00:00" },
+          userUid: "",
+        },
+      ],
+      allCount: 1,
+    };
+    const dispatched = [];
+    const getOrdersNextApi = jest
+      .spyOn(api, "getOrdersNextAPI")
+      .mockImplementation(() => Promise.resolve(fakeOrdersData));
+
+    await runSaga(
+      {
+        dispatch: action => dispatched.push(action),
+        getState: () => ({ orders: { limit: 4, query: "query", orders: [] } }),
+      },
+      fetchOrdersNext,
+      []
+    );
+
+    expect(getOrdersNextApi).toHaveBeenCalledTimes(1);
+    expect(dispatched).toEqual([fetchOrdersNextSucceed(fakeOrdersData)]);
+
+    getOrdersNextApi.mockClear();
+  });
+
+  it("should fetch orders next and dispatch error action", async () => {
+    const dispatched = [];
+    const getOrdersNextApi = jest
+      .spyOn(api, "getOrdersNextAPI")
+      .mockImplementation(() => Promise.reject());
+
+    await runSaga(
+      {
+        dispatch: action => dispatched.push(action),
+        getState: () => ({ orders: { limit: 4, query: "query", orders: [] } }),
+      },
+      fetchOrdersNext,
+      []
+    );
+
+    expect(getOrdersNextApi).toHaveBeenCalledTimes(1);
+    expect(dispatched).toEqual([fetchOrdersNextFailed()]);
+
+    getOrdersNextApi.mockClear();
+  });
+});
+
+describe("cancelOrder saga", () => {
+  it("should cancel order and dispatch success action", async () => {
+    const dispatched = [];
+    const cancelOrderApi = jest
+      .spyOn(api, "cancelOrderAPI")
+      .mockImplementation(() => Promise.resolve());
+
+    await runSaga(
+      {
+        dispatch: action => dispatched.push(action),
+      },
+      cancelOrder,
+      []
+    );
+
+    expect(cancelOrderApi).toHaveBeenCalledTimes(1);
+    expect(dispatched).toEqual([cancelOrderSucceed(), fetchOrdersInitialRequested()]);
+
+    cancelOrderApi.mockClear();
+  });
+
+  it("should cancel order and dispatch error action", async () => {
+    const dispatched = [];
+    const cancelOrderApi = jest
+      .spyOn(api, "cancelOrderAPI")
+      .mockImplementation(() => Promise.reject());
+
+    await runSaga(
+      {
+        dispatch: action => dispatched.push(action),
+      },
+      cancelOrder,
+      []
+    );
+
+    expect(cancelOrderApi).toHaveBeenCalledTimes(1);
+    expect(dispatched).toEqual([cancelOrderFailed()]);
+
+    cancelOrderApi.mockClear();
   });
 });

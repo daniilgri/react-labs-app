145d672955857540196a906db181b7f5427f8b46
Daniil Grishaev <grishaev.daniil1@gmail.com>
2020-12-25 16:11:44 +0300

    date and time schema validation for addScreeningDate modal

--- src/validations/addScreeningDateSchema.js
+++ src/validations/addScreeningDateSchema.js
@@ -1,6 +1,12 @@
 import * as Yup from "yup";
 
 export default Yup.object().shape({
-  date: Yup.string().required("Required"),
-  times: Yup.array().of(Yup.string().required("Required")).required("Required"),
+  date: Yup.date().typeError("Invalid date format"),
+  times: Yup.array()
+    .of(
+      Yup.string()
+        .matches(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/, "Invalid time format")
+        .required("Required")
+    )
+    .required("Required"),
 });

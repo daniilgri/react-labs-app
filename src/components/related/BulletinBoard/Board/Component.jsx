80c3335b45d8bf8e2ab0b27be7979265e5b035f7
Daniil Grishaev <grishaev.daniil1@gmail.com>
2020-12-29 17:16:16 +0300

    stories for searchBar global component

--- src/components/related/BulletinBoard/Board/Component.jsx
+++ src/components/related/BulletinBoard/Board/Component.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
 
+import SearchBar from "../../../global/SearchBar";
 import Card from "./Card";
-import { Wrapper, Loading, FetchButton, CenterContainer } from "./styles";
+import { Wrapper, Loading, FetchButton, CenterContainer, BoardContainer } from "./styles";
 
 const Component = ({
   loading,
@@ -13,6 +14,8 @@ const Component = ({
   fetchFilmsNextRequested,
   allCount,
   count,
+  setFilmsSearchQuery,
+  query,
 }) => {
   const handleGetMoreButtonOnClick = event => {
     event.preventDefault();
@@ -28,21 +31,24 @@ const Component = ({
 
   return (
     <>
-      <Wrapper>
-        {films.length > 0 &&
-          films.map(item => (
-            <Card key={uuidv4()} title={item.title} id={item.id} image={item.image} />
-          ))}
-      </Wrapper>
-      {loading || error ? (
-        <Loading>Loading</Loading>
-      ) : (
-        count < allCount && (
-          <CenterContainer>
-            <FetchButton onClick={handleGetMoreButtonOnClick}>Get more</FetchButton>
-          </CenterContainer>
-        )
-      )}
+      <SearchBar value={query} onSet={setFilmsSearchQuery} />
+      <BoardContainer>
+        <Wrapper>
+          {films.length > 0 &&
+            films.map(item => (
+              <Card key={uuidv4()} title={item.title} id={item.id} image={item.image} />
+            ))}
+        </Wrapper>
+        {loading || error ? (
+          <Loading>Loading</Loading>
+        ) : (
+          count < allCount && (
+            <CenterContainer>
+              <FetchButton onClick={handleGetMoreButtonOnClick}>Get more</FetchButton>
+            </CenterContainer>
+          )
+        )}
+      </BoardContainer>
     </>
   );
 };
@@ -50,6 +56,7 @@ const Component = ({
 Component.defaultProps = {
   films: [],
   error: "",
+  query: "",
 };
 
 Component.propTypes = {
@@ -60,6 +67,8 @@ Component.propTypes = {
   fetchFilmsNextRequested: PropTypes.func.isRequired,
   allCount: PropTypes.number.isRequired,
   count: PropTypes.number.isRequired,
+  setFilmsSearchQuery: PropTypes.func.isRequired,
+  query: PropTypes.string,
 };
 
 export default Component;
--- src/components/related/BulletinBoard/Board/styles.js
+++ src/components/related/BulletinBoard/Board/styles.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const BoardContainer = styled.div`
+  margin-bottom: 40px;
+  max-width: 85%;
+  margin: 0 auto;
+  margin-top: 40px;
+`;
+
 export const Wrapper = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));

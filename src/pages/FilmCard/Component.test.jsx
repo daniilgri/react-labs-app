90943203df5af93a04e92096dac252f3701153d4
Daniil Grishaev <grishaev.daniil1@gmail.com>
2020-12-30 10:25:15 +0300

    Snapshots for FilmCard and Signup pages

---
+++ src/pages/FilmCard/Component.test.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+
+import Component from "./Component";
+
+const mockStore = configureStore([]);
+
+describe("FilmCard component", () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      auth: {
+        loggedIn: false,
+        auth: {},
+      },
+      filmCard: {
+        loading: false,
+        error: "",
+      },
+      newOrder: {
+        error: "",
+        loading: false,
+      },
+    });
+  });
+
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Router>
+            <Component />
+          </Router>
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
